c1113e3784871db681e5777cdec811d26bcdbb48
Zach Harris <z@zharr.is>
2018-03-27 14:12:23 -0400

    create border component

--- src/components/Border.js
+++ src/components/Border.js
@@ -1,25 +1,11 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import Backslash from '../../styles/backslash'
+import Tilde from '../../styles/tilde'
 
 // Add Page title as Prop
 const Header = () => (
-  <div
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1>Ƶ</h1>
-      <Backslash />
-    </div>
+  <div>
+    <h1>Ƶ</h1>
+    <Tilde />
   </div>
 )
-
 export default Header
