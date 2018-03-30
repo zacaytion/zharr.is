ad2c380117db85b6793d74484afd5c0bed64d6dd
Zach Harris <z@zharr.is>
2018-03-30 14:18:36 -0400

    create skelton of layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,14 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import styled from 'react-emotion'
-import Header from '../components/Header'
 import globals from '../styles/global'
+import Border from '../components/Border'
+import Header from '../components/Header'
+import Navigation from '../components/Nav'
+import Tilde from '../styles/tilde'
 
 const Container = styled.div`
+  margin-top: 20px;
+  margin-left: 20px;
   min-height: 100vh;
+  display: grid;
+  grid-template-columns: 20px auto;
+  grid-template-rows: 50px 50px auto;
+  grid-gap: 10px;
 `
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({ location, children }) => (
   <Container>
     <Helmet
       title="Gatsby Default Starter"
@@ -18,21 +27,17 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Border />
+    <Navigation />
+    <div />
+    <Header title={location.pathname} />
+    <Tilde />
+    <div>{children()}</div>
   </Container>
 )
 
 TemplateWrapper.propTypes = {
+  location: PropTypes.object,
   children: PropTypes.func,
 }
 
