13255c159b22d7babf08ef25f2eff37492fb5443
Zach Harris <z@zharr.is>
2018-03-30 14:18:01 -0400

    build tilde component

--- src/styles/tilde.js
+++ src/styles/tilde.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import { noSelect } from './utils'
+
 // TODO: Create a Tilde with SVG
+const Tilde = () => (
+  <p className={noSelect}>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  </p>
+)
+
+export default Tilde
