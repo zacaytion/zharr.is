d5e262cf0c8cc6268828dbbf76f0c6099f2dee27
Zach Harris <z@zharr.is>
2018-03-31 17:05:56 -0400

    lowercase

--- data/config.js
+++ data/config.js
@@ -13,7 +13,7 @@ module.exports = {
   siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
-  postDefaultCategoryID: 'Tech', // Default category for posts.
+  postDefaultCategoryID: 'tech', // Default category for posts.
   userName: 'Zachary Harris', // Username to display in the author segment.
   userTwitter: '', // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: 'North Pole, Earth', // User location to display in the author segment.
