62df733ffdf952b577a45fc88b8a8712a86b7f59
Zach Harris <z@zharr.is>
2018-04-13 12:01:18 -0400

    add code syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,17 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {
+                ts: 'typescript',
+                js: 'javascript'
+              },
+            }
+          },
+          {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 690,
@@ -47,7 +58,6 @@ module.exports = {
           {
             resolve: 'gatsby-remark-responsive-iframe',
           },
-          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-autolink-headers',
           'gatsby-remark-smartypants',
