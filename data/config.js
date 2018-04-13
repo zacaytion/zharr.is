c47a1e9c0565cbef64c90be2ce8f05b0d43764f7
Zach Harris <z@zharr.is>
2018-04-13 12:01:02 -0400

    update meta-data

--- data/config.js
+++ data/config.js
@@ -5,18 +5,17 @@
 module.exports = {
   blogPostDir: 'posts', // The name of directory that contains your posts.
   siteTitle: 'Hello World!', // Site title.
-  siteTitleAlt: 'Zachary Harris', // Alternative site title for SEO.
+  siteTitleAlt: 'Zach Harris', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
   siteUrl: 'https://zharr.is', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  // TODO: Change site description
-  siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Hello, World!', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
   postDefaultCategoryID: 'tech', // Default category for posts.
-  userName: 'Zachary Harris', // Username to display in the author segment.
-  userTwitter: '', // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: 'North Pole, Earth', // User location to display in the author segment.
+  userName: 'Zach Harris', // Username to display in the author segment.
+  userTwitter: 'zacaytion', // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: 'Earth', // User location to display in the author segment.
   userDescription: '', // Links to social profiles/projects you want to display in the author segment/navigation bar.
   navPaths: [
     {
@@ -32,16 +31,16 @@ module.exports = {
       path: '/blog',
     },
     {
-      name: 'Now',
-      path: '/now',
-    },
-    {
       name: 'Projects',
       path: '/projects',
     },
     {
       name: 'Reading',
       path: '/reading',
+    },
+    {
+      name: 'Now',
+      path: '/now',
     },
   ],
   userLinks: [
