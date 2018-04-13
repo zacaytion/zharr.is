c0e635b25963c71ae3905cf359d9ef2c6db7e12b
Zach Harris <z@zharr.is>
2018-04-13 12:07:02 -0400

    tweak layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,50 +8,55 @@ import Z from '../components/Z'
 import Header from '../components/Header'
 import Navigation from '../components/Nav'
 import Tilde from '../styles/tilde'
+import Footer from '../components/Footer'
+require("prismjs/themes/prism-tomorrow.css")
 
 const Container = styled.div`
-  max-width: 1200px;
-  max-height: 98vh;
-  overflow-x: hidden;
-  padding: 10px 10px; 
+    max-width: 1200px;
+    max-height: 105vh;
+    overflow-x: hidden;
+    padding: 10px 10px; 
 `
 
 const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 3% 97%;
-  grid-template-rows: 15% 10% auto;
-  grid-row-gap: 10px;
-  grid-column-gap: 20px;
-  grid-template-areas:
+    display: grid;
+    grid-template-columns: 2% 98%;
+    grid-template-rows: 15% 10% auto;
+    grid-row-gap: 10px;
+    grid-column-gap: 20px;
+    grid-template-areas:
     'logo navigation'
     '. header'
     'tilde content';
 `
 const Content = styled.div`
-  grid-area: content;
-  padding-right: 20px;
-  padding-left: 5px;
-  max-height: 71vh;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  overflow-wrap: break-word;
+    grid-area: content;
+    padding-right: 65px;
+    padding-left: 5px;
+    max-height: 85vh;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    overflow-wrap: break-word;
 `
 
- 
+
 const TemplateWrapper = ({ location, children }) => (
-  <Container>
-    <Helmet
-      title={config.siteDescription}
-      meta={[{ name: 'description', content: config.siteDescription }]}
-    />
-    <Grid>
-      <Z />
-      <Navigation path={location.pathname} />
-      <Header title={location.pathname} />
-      <Tilde />
-      <Content>{children()}</Content>
-    </Grid>
-  </Container>
+    <Container>
+        <Helmet
+            title={config.siteDescription}
+            meta={[{ name: 'description', content: config.siteDescription }]}
+        />
+        <Grid>
+            <Z />
+            <Navigation path={location.pathname} />
+            <Header title={location.pathname} />
+            <Tilde />
+            <Content>
+                {children()}
+                <Footer/>
+            </Content>
+        </Grid>
+    </Container>
 )
 
 TemplateWrapper.propTypes = {
