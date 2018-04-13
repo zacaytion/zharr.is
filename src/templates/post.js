86e57fd7b223bf2643dc365eeef0611d93c0bbe3
Zach Harris <z@zharr.is>
2018-04-13 12:06:49 -0400

    add padding

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,7 +4,11 @@ import SEO from '../components/SEO'
 import config from '../../data/config'
 import PostCategory from '../components/Post/Category'
 import PostTags from '../components/Post/Tags'
+import styled from 'react-emotion'
 
+const Div = styled.div`
+  padding: 0 15px;
+`
 export default ({ pathContext, data }) => {
   const { slug } = pathContext
   const postNode = data.markdownRemark
@@ -13,20 +17,18 @@ export default ({ pathContext, data }) => {
   post.category = post.category || config.postDefaultCategoryID
 
   return (
-    <div>
+    <Div>
       <Helmet>
         <title>{`${post.title} | ${config.siteTitle}`}</title>
       </Helmet>
       <SEO postPath={slug} postNode={postNode} postSEO />
-      <div>
         <h1>{post.title}</h1>
         <PostCategory category={post.category} />
         <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
         <div className="post-meta">
           <PostTags tags={post.tags} />
         </div>
-      </div>
-    </div>
+    </Div>
   )
 }
 
