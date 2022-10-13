626f27d2036e965ece8266d049e2ea13b1622c37
Zach Harris <code@zharr.is>
2022-10-13 18:44:47 -0400

    feat: init hugo site from scratch

--- src/components/Link.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { Link } from "gatsby";
-
-export default props => <Link href={props.to} {...props} />;
--- src/components/Post/Category.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Link from '../Link'
-
-export default ({ category }) => (
-  <Link key={category} to={`/categories/${category}`}>
-    Category - {category}
-  </Link>
-)
